4bf89946fa1876406c5fc381c9a1ce23be439eb6
Marc Villacorta <marc.villacorta@gmail.com>
2015-01-17 17:40:18 +0100

    Adding coreup script

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
 class r_kvm {
 
   contain ::r_base
+
+  file { '/usr/local/sbin/coreup':
+    ensure  => present,
+    content => template("${module_name}/coreup.erb"),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
 }
