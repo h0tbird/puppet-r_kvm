b53e3abfb9fa17da45eeb4639e82e8bc85268873
Marc Villacorta <marc.villacorta@gmail.com>
2015-06-03 14:18:58 +0200

    Added initial docker service setup

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -1,2 +1,32 @@
 class r_kvm::docker {
+
+  package { 'docker':
+    ensure => latest,
+  } ->
+
+  file {
+
+    '/etc/sysconfig/docker':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644';
+
+    '/etc/sysconfig/docker-storage':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644';
+
+    '/etc/sysconfig/docker-network':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644';
+  } ->
+
+  service { 'docker':
+    ensure => 'running',
+    enable => true,
+  }
 }
