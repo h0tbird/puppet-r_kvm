156bd955777a01cf74024d86c97fafb1041cd508
Marc Villacorta <marc.villacorta@gmail.com>
2015-06-15 12:26:04 +0200

    Added initial configuration

--- manifests/booddies.pp
+++ manifests/booddies.pp
@@ -1,6 +1,26 @@
 class r_kvm::booddies {
 
+  #----------------------
+  # Install the package:
+  #----------------------
+
   package { 'booddies':
     ensure => latest,
+  }
+
+  #-------------------------
+  # Configure the services:
+  #-------------------------
+
+  ['boot','cgit','data','gito','regi'].each |$file| {
+
+    file { "/etc/booddies/${file}.conf":
+      ensure  => file,
+      content => template("${module_name}/${file}.conf.erb"),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package['booddies'];
+    }
   }
 }
