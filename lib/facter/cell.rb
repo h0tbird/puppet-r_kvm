77605d5cc67fb3c47ac165291600aa31f2967d41
Marc Villacorta <marc.villacorta@gmail.com>
2015-06-15 15:25:00 +0200

    Convert string to integer

--- lib/facter/cell.rb
+++ lib/facter/cell.rb
@@ -1,7 +1,7 @@
 if Facter.value(:kernel) == 'Linux'
   Facter.add('cell') do
     setcode do
-      %x{cat /proc/sys/kernel/hostname | sed -r 's/.*cell-([0-9]*).*/\\1/'}
+      %x{cat /proc/sys/kernel/hostname | sed -r 's/.*cell-([0-9]*).*/\\1/'}.to_i
     end
   end
 end
