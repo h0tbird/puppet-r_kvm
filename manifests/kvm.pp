02554d1cecd9305c212117e3f635f55545460c8a
Marc Villacorta <marc.villacorta@gmail.com>
2015-12-29 11:49:47 +0100

    Do not merge pages across NUMA nodes

--- manifests/kvm.pp
+++ manifests/kvm.pp
@@ -8,19 +8,35 @@ class r_kvm::kvm {
     ensure => present,
   }
 
+
   #--------------------------
   # Kernel samepage merging:
   #--------------------------
 
   if hiera('KSM') {
 
-    file { '/etc/ksmtuned.conf':
-      ensure  => present,
-      content => template("${module_name}/kvm/ksmtuned.conf.erb"),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      require => Package['qemu-system-x86'],
+    file {
+
+      '/etc/systemd/system/ksm.service.d':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755';
+
+      '/etc/systemd/system/ksm.service.d/50-dont-merge-across.conf':
+        ensure  => present,
+        content => template("${module_name}/kvm/50-dont-merge-across.conf.erb"),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644';
+
+      '/etc/ksmtuned.conf':
+        ensure  => present,
+        content => template("${module_name}/kvm/ksmtuned.conf.erb"),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['qemu-system-x86'];
     } ~>
 
     service { ['ksm', 'ksmtuned']:
